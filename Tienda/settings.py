0d405e5e7ef17802b0c8065982ed5995453dfb30
vzapatag5 <vzapatag5@eafit.edu.co>
2026-04-21 15:08:12 -0500

    cambio en settings.py

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -71,6 +71,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
